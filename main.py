78c8567fc074f726740de468f7a52c9d661b3165
Michael Hels <mike3hels@gmail.com>
2025-09-19 19:02:29 -0400

    added letter count method

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
+from stats import word_count, character_count
+
 def get_book_text(filepath):
     with open(filepath) as f:
         file_contents = f.read()
         return file_contents
     
+
+    
 def main():
-    print(get_book_text('books/frankenstein.txt'))
+    path = 'books/frankenstein.txt'
+    book_text = get_book_text(path)
+    #word_count(book_text)
+    character_count(book_text)
     
 
 main()
